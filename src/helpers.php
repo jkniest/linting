39813bc9554a1175c2de715c300ea3a4985060e4
Jordan Kniest <mail@jkniest.de>
2021-02-22 00:25:05 +0100

    Initial

---
+++ src/helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace jkniest\Linting;
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder; 
+
+
+function styles(Finder $finder, array $rules = []): Config {
+  $rules = array_merge(require __DIR__.'/rules.php', $rules);
+
+  return Config::create()
+    ->setFinder($finder)
+    ->setRiskyAllowed(true)
+    ->setRules($rules);
+}
