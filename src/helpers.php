6c015f52cf73d0809c9c1cd3c463d46863f2e3dd
Jordan Kniest <mail@jkniest.de>
2025-08-29 10:52:36 +0200

    Update php cs fixer (#41)

--- src/helpers.php
+++ src/helpers.php
@@ -1,17 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace jkniest\Linting;
 
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
 
-function styles(Finder $finder, array $rules = []): Config {
-  $rules = array_merge(require __DIR__.'/rules.php', $rules);
+function styles(Finder $finder, array $rules = []): Config
+{
+    $rules = array_merge(require __DIR__.'/rules.php', $rules);
 
-  return (new Config())
-    ->setFinder($finder)
-    ->setRiskyAllowed(true)
-    ->setRules($rules)
-    ->setParallelConfig(ParallelConfigFactory::detect());
+    return new Config()
+        ->setFinder($finder)
+        ->setRiskyAllowed(true)
+        ->setRules($rules)
+        ->setParallelConfig(ParallelConfigFactory::detect());
 }
